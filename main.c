e8d3c00b9a0e1b9302c45a9ec4b5f2ef12ca859d
Dylan Slogrove <dslogrove@gmail.com>
2018-08-01 18:05:08 +0200

    Fixed init memleak

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 17:16:43 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/08/01 12:24:03 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/08/01 18:04:57 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 static char	**init_env(char **env)
 {
-	char **ret;
+	char	**ret;
+	char	*newlvl;
 
 	ret = ft_tabdup(env);
 	if (!ft_getenv("PATH", ret))
 		default_path(&ret);
 	ft_setenv("SHELL", ft_getenv("_", ret), &ret);
-	ft_setenv("SHLVL", ft_itoa(ft_atoi(ft_getenv("SHLVL", ret)) + 1), &ret);
+	newlvl = ft_itoa(ft_atoi(ft_getenv("SHLVL", ret)) + 1);
+	ft_setenv("SHLVL", newlvl, &ret);
+	free(newlvl);
 	return (ret);
 }
 
@@ -105,7 +108,6 @@ int			main(int argc, char *argv[], char *envv[])
 	(void)(argv && argc);
 	env = init_env(envv);
 	status = 0;
-	signal(SIGINT, SIG_IGN);
 	while (1)
 	{
 		prompt(env);
